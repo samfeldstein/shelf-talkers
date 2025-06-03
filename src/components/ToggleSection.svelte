fc120e5bb7ea6f98430146cec9d5e24fed4201ef
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-06-03 14:20:23 -0600

    Style options button

---
+++ src/components/ToggleSection.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let label = "Options";
+  export let buttonClass;
+
+  let isVisible = false;
+
+  function toggle() {
+    isVisible = !isVisible;
+  }
+</script>
+
+<button on:click={toggle} class={buttonClass}>
+  {label}
+</button>
+
+{#if isVisible}
+  <div class="toggle-content">
+    <slot />
+  </div>
+{/if}
+
+<style>
+  .toggle-content {
+    margin-top: 1rem;
+  }
+</style>
