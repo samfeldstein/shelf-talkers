d17a10c0831bc457576531e084588278eaf9a8be
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-06-27 11:11:17 -0600

    Update help page

--- src/scripts/options/clear.js
+++ src/scripts/options/clear.js
@@ -2,25 +2,31 @@ import close from "../utils/close"
 
 clearAllCards()
 
+// Clear cards
 function clearAllCards() {
   const button = document.getElementById('clearAll')
 
   button.addEventListener('click', function () {
-    const cards = document.querySelectorAll('.card')
-    cards.forEach(card => {
-      card.querySelector('.title').innerHTML = ''
-      card.querySelector('.author').innerHTML = ''
-      card.querySelector('.blurb').innerHTML = ''
-      card.querySelector('.submitter').innerHTML = ''
-    });
-
+    clearFields()
     localStorage.removeItem('allShelfTalkerData')
-
+    const box = button.closest('.options-box')
+    close(box)
     closeMenu()
   })
 }
 
+// Clear fields
+function clearFields() {
+  const cards = document.querySelectorAll('.card')
+  cards.forEach(card => {
+    card.querySelector('.title').innerHTML = ''
+    card.querySelector('.author').innerHTML = ''
+    card.querySelector('.blurb').innerHTML = ''
+    card.querySelector('.submitter').innerHTML = ''
+  });
+}
 
+// Close menu
 function closeMenu() {
   const optionsMenu = document.getElementById('optionsDiv')
   close(optionsMenu)
