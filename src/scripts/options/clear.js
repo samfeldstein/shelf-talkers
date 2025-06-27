3ea44ed4794521ca79e472527b3b5316f7325b69
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-06-26 18:24:11 -0600

    Add clear all button logic

--- src/scripts/options/clear.js
+++ src/scripts/options/clear.js
@@ -1,3 +1,27 @@
-// delete card data from local storage
+import close from "../utils/close"
 
-const clearAllButton = document.getElementById("clearAll");
+clearAllCards()
+
+function clearAllCards() {
+  const button = document.getElementById('clearAll')
+
+  button.addEventListener('click', function () {
+    const cards = document.querySelectorAll('.card')
+    cards.forEach(card => {
+      card.querySelector('.title').innerHTML = ''
+      card.querySelector('.author').innerHTML = ''
+      card.querySelector('.blurb').innerHTML = ''
+      card.querySelector('.submitter').innerHTML = ''
+    });
+
+    localStorage.removeItem('allShelfTalkerData')
+
+    closeMenu()
+  })
+}
+
+
+function closeMenu() {
+  const optionsMenu = document.getElementById('optionsDiv')
+  close(optionsMenu)
+}
