554591e854c698b3865210dea6030151311a93ae
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-09-11 15:55:09 -0600

    Add clear cards button

---
+++ src/components/ClearCards.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useCards } from '@/composables/useCards'
+const { cards } = useCards()
+
+function clearCards() {
+  cards.value = []
+}
+</script>
+
+<template>
+  <button class="default" @click="clearCards">Clear Cards</button>
+</template>
