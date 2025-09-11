a9490f6148550200b2f51e19c9f3f35f4ad61718
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-09-11 09:38:57 -0600

    Style modal

--- src/components/CreateCardForm.vue
+++ src/components/CreateCardForm.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive, ref, nextTick } from "vue";
+import Modal from "./Modal.vue";
 
 const emit = defineEmits(["create"]);
 
@@ -36,109 +37,45 @@ function handleSubmit() {
     });
   }
 }
-
-// Toggle form
-const showForm = ref(false);
-
-function toggleForm() {
-  showForm.value = !showForm.value;
-  if (showForm.value) {
-    nextTick(() => {
-      titleInput.value?.focus();  
-    });
-  }
-}
 </script>
 
 <template>
-  <button class="show" type="button" @click="toggleForm">Create Card</button>
-  <form
-    class="box-shadow"
-    v-show="showForm"
-    @submit.prevent="handleSubmit"
-    aria-labelledby="form-title"
-  >
-    <fieldset>
-      <legend id="form-title">Create Talker</legend>
+  <Modal label="Create Talker">
+    <form @submit.prevent="handleSubmit" aria-labelledby="form-title">
+      <fieldset>
+        <legend id="form-title">Create Talker</legend>
 
-      <div>
-        <label for="title-input">Title</label>
-        <input
-          id="title-input"
-          ref="titleInput"
-          v-model="form.title"
-          placeholder="The Road"
-          required
-          autocomplete="off"
-        />
-      </div>
+        <div>
+          <label for="title-input">Title</label>
+          <input id="title-input" ref="titleInput" v-model="form.title" placeholder="The Road" required
+            autocomplete="off" />
+        </div>
 
-      <div>
-        <label for="author-input">Author</label>
-        <input
-          id="author-input"
-          v-model="form.author"
-          placeholder="Cormac McCarthy"
-          required
-          autocomplete="off"
-        />
-      </div>
+        <div>
+          <label for="author-input">Author</label>
+          <input id="author-input" v-model="form.author" placeholder="Cormac McCarthy" required autocomplete="off" />
+        </div>
 
-      <div>
-        <label for="blurb-input">Blurb</label>
-        <textarea
-          id="blurb-input"
-          v-model="form.blurb"
-          placeholder="Woof"
-          required
-          autocomplete="off"
-        ></textarea>
-      </div>
+        <div>
+          <label for="blurb-input">Blurb</label>
+          <textarea id="blurb-input" v-model="form.blurb" placeholder="Woof" required autocomplete="off"></textarea>
+        </div>
 
-      <div>
-        <label for="attribution-input">Attribution</label>
-        <input
-          id="attribution-input"
-          v-model="form.attribution"
-          placeholder="Sam, Bookseller at Composition Shop"
-          required
-          autocomplete="off"
-        />
-      </div>
+        <div>
+          <label for="attribution-input">Attribution</label>
+          <input id="attribution-input" v-model="form.attribution" placeholder="Sam, Bookseller at Composition Shop"
+            required autocomplete="off" />
+        </div>
 
-      <button type="submit" :disabled="success">
-        {{ success ? "Talker Created ✓" : "Create Talker" }}
-      </button>
-    </fieldset>
-  </form>
-  <button
-    type="button"
-    class="close-button"
-    @click="showForm = false"
-    aria-label="Close form"
-  >
-    &times;
-  </button>
+        <button class="submit" type="submit" :disabled="success">
+          {{ success ? "Talker Created ✓" : "Create Talker" }}
+        </button>
+      </fieldset>
+    </form>
+  </Modal>
 </template>
 
 <style lang="scss" scoped>
-form {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  background: white;
-  width: 100%;
-  height: 100dvh;
-  border-radius: 4px;
-  padding: 1rem;
-}
-
-fieldset {
-  max-width: 40rem;
-  margin: 0 auto;
-}
-
 div {
   display: grid;
   gap: 0.5rem;
@@ -147,5 +84,9 @@ div {
 label {
   font-weight: 700;
   font-size: 1rem;
+}
+
+button.submit {
+  width: fit-content;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+defineProps(['label'])
 
 const open = ref(false);
 const dialogRef = ref(null);
@@ -16,10 +17,35 @@ function closeModal() {
 </script>
 
 <template>
-  <button @click="openModal">Open Modal</button>
+  <button @click="openModal">{{ label }}</button>
 
   <dialog ref="dialogRef">
-    <slot />
-    <button @click="closeModal">Close</button>
+    <div class="content">
+
+
+      <slot />
+
+      <button class="close" @click="closeModal">Close</button>
+    </div>
   </dialog>
 </template>
+
+<style scoped>
+dialog {
+  width: 100%;
+  height: 100%;
+  border: 1px solid currentColor;
+  border-radius: 4px;
+}
+
+.content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+button.close {
+  width: fit-content;
+}
+</style>
