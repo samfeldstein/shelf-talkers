26fe74ab8a226858c6e1280ad48052c1208414f4
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-10-19 22:03:44 -0600

    Set explicit logo height

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -10,6 +10,7 @@ const { logoSrc, showLogo } = useLogo();
 
 <style lang="scss" scoped>
 img {
+  height: 100%; // Set explicit width to improve page load
   max-height: 2.75em;
   width: auto;
   flex: 0; // Otherwise, grows to fill space on Safari and mobile, making title and author wrap. Title and author (hgroup) have flex 1
